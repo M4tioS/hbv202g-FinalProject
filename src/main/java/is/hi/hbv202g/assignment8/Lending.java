cb4544a0b4511aadcfe72d78b27c60aa82526f99
MatioS <dkm2@hi.is>
2025-04-02 10:18:36 +0000

    Added documentatino for Omnibus and Lending

--- src/main/java/is/hi/hbv202g/assignment8/Lending.java
+++ src/main/java/is/hi/hbv202g/assignment8/Lending.java
@@ -1,40 +1,79 @@
 package is.hi.hbv202g.assignment8;
 
 import java.time.LocalDate;
+
+/**
+ * Represents a lending transaction in the library system.
+ * A lending consists of a book, a user who borrowed it, and a due date.
+ */
 public class Lending {
     private LocalDate dueDate;
     private Book book;
     private User user;
 
+    /**
+     * Creates a new lending with the specified book and user.
+     * The due date is automatically set to 30 days from now.
+     * 
+     * @param book The book being borrowed
+     * @param user The user borrowing the book
+     */
     public Lending(Book book, User user) {
         this.dueDate = LocalDate.now().plusDays(30);
         this.book = book;
         this.user = user;
-
     }
 
-
-
+    /**
+     * Gets the due date for returning the book.
+     * 
+     * @return The due date
+     */
     public LocalDate getDueDate() {
         return dueDate;
     }
 
+    /**
+     * Sets the due date for returning the book.
+     * 
+     * @param dueDate The new due date
+     */
     public void setDueDate(LocalDate dueDate) {
         this.dueDate = dueDate;
     }
 
+    /**
+     * Gets the book in this lending.
+     * 
+     * @return The book
+     */
     public Book getBook() {
         return book;
     }
 
+    /**
+     * Sets the book in this lending.
+     * 
+     * @param book The new book
+     */
     public void setBook(Book book) {
         this.book = book;
     }
 
+    /**
+     * Gets the user who borrowed the book.
+     * 
+     * @return The user
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Sets the user who borrowed the book.
+     * 
+     * @param user The new user
+     */
     public void setUser(User user) {
         this.user = user;
     }
--- src/main/java/is/hi/hbv202g/assignment8/Omnibus.java
+++ src/main/java/is/hi/hbv202g/assignment8/Omnibus.java
@@ -2,30 +2,66 @@ package is.hi.hbv202g.assignment8;
 
 import java.util.List;
 
+/**
+ * Represents an omnibus in the library system.
+ * An omnibus is a collection of multiple books combined in a set.
+ * This implements the Composite pattern, where an omnibus contains multiple books.
+ */
 public class Omnibus {
     private List<Book> books;
 
-
+    /**
+     * Creates a new omnibus with the specified list of books.
+     * 
+     * @param books The list of books in the omnibus
+     */
     public Omnibus(List<Book> books) {
         this.books = books;
     }
 
+    /**
+     * Gets the list of books in the omnibus.
+     * 
+     * @return The list of books
+     */
     public List<Book> getBooks() {
         return books;
     }
 
+    /**
+     * Sets the list of books in the omnibus.
+     * 
+     * @param books The new list of books
+     */
     public void setBooks(List<Book> books) {
         this.books = books;
     }
 
+    /**
+     * Adds a book to the omnibus.
+     * 
+     * @param book The book to add
+     */
     public void addBook(Book book) {
         this.books.add(book);
     }
 
+    /**
+     * Removes a book from the omnibus.
+     * 
+     * @param book The book to remove
+     */
     public void removeBook(Book book) {
         this.books.remove(book);
     }
 
+    /**
+     * Finds a book in the omnibus by its title.
+     * 
+     * @param title The title to search for
+     * @return The found book
+     * @throws UserOrBookDoesNotExistException if no book with the title exists in the omnibus
+     */
     public Book findBookByTitle(String title) throws UserOrBookDoesNotExistException {
         for (Book book : books) {
             if (book.getTitle().equals(title)) {
@@ -34,7 +70,4 @@ public class Omnibus {
         }
         throw new UserOrBookDoesNotExistException("Book with title " + title + " does not exist");
     }
-
-
-
 }
